8e98a5d1036aa08bf406809c511ca3b6b2901ebc
leahkathryn <66423163+leahkathryn@users.noreply.github.com>
2023-11-12 11:46:10 -0600

    Fixed the error in the Gradle build

---
+++ src/test/com/ExampleUnitTestFile.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+// an example of a possible structure of a JUNIT test
+public class ExampleUnitTestFile
+{
+    @BeforeEach
+    public void setUp() throws Exception {}
+
+    @AfterEach
+    public void tearDown() throws Exception {}
+
+    @Test
+    public void testSomething() {
+        fail("Not implemented");
+    }
+}
