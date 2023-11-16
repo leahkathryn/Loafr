2417bc497cb5227f4b6e89ef280700c6e1cff737
Ken-Likit <88905005+Ken-Likit@users.noreply.github.com>
2023-11-16 17:59:41 -0600

    search implementation

--- src/main/java/com/analyze/Analyzer.java
+++ src/main/java/com/analyze/Analyzer.java
@@ -4,10 +4,18 @@ import java.util.ArrayList;
 
 public class Analyzer {
     //private AnalysisTask queue[];
+    //questions i have
+    //where do i find output file location
+    //how do i interact with logevent
+    //how do i create logobject
+    private LogObject output;
+
     private ArrayList<AnalysisTask> queue = new ArrayList<AnalysisTask>();
 
     public analyze(LogObject analyzeLogObject) {
-        //execute Analysistask on analyzeLogobject
+        AnalysisTask temp = new Search(type, data, reg);
+        temp.execute(analyzeLogObject);
+        output = temp.outputEvents;
     }
 
     public addToQueue(AnalysisTask newTask) {
--- src/main/java/com/analyze/Search.java
+++ src/main/java/com/analyze/Search.java
@@ -1,10 +1,16 @@
 package com.analyze;
 
+import java.util.List;
+
 public class Search implements AnalysisTask {
 
     private AttributeType attributeType;
     private DataID dataID;
     private String regex;
+    private List<LogEvents> events;
+    private List<LogEvents> outputEvents;
+    private LogEvents temp;
+
 
     public Search(AttributeType type, DataID data, String reg) {
         attributeType = type;
@@ -14,15 +20,25 @@ public class Search implements AnalysisTask {
 
     public Search(AttributeType type, String reg) {
         attributeType = type;
+        //dataID = ?;
         regex = reg;
     }
 
     public Search(String reg) {
+        //attributeType = ?;
+        //dataID = ?;
         regex = reg;
     }
 
+    //what functions can i use with logevents,
     @Override
     public void execute(LogObject logObject) {
-
+        events = logObject.getLogEvents();
+        for (int i = 0; i < events.size(); i++) {
+            temp = events.get(i);
+            if (temp.get(dataID) == regex) {
+                outputEvents.add(temp);
+            }
+        }
     }
 }
