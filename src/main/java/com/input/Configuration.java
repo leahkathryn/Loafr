0e6d9f0c522db457bbf67d664da4d7077715e07f
AwesomeNova <coolnova45@gmail.com>
2023-11-17 16:31:49 -0600

    Revert "Leah needed a getter for the dataIDList."
    This reverts commit be8ca47d69b395b8e021b8baed8fd6108ecec250.

--- src/main/java/com/input/Configuration.java
+++ src/main/java/com/input/Configuration.java
@@ -1,8 +1,10 @@
 package com.input;
 
 import com.ErrorHandler;
-
-import org.w3c.dom.*;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilder;
