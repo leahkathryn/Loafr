2e292639a0f2d101c0f0aa8d0ec643e8b98b2172
leahkathryn <66423163+leahkathryn@users.noreply.github.com>
2023-11-17 20:40:30 -0600

    Test framework setup
    I just created one test class for each package so that the structure would be clear. Each Loafr class that requires testing should have one corresponding test class, with as many test methods as you feel is necessary.

---
+++ src/test/java/com/interpret/InterpreterTest.java
@@ -0,0 +1,19 @@
+package com.interpret;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class InterpreterTest
+{
+    @BeforeAll
+    static void beforeAll()
+    {
+        // test class set up
+    }
+
+    // test case naming convention: MethodName_StateUnderTest_ExpectedBehavior
+    @Test
+    void MethodName_StateUnderTest_ExpectedBehavior() {
+        System.out.println("**--- Test method1 executed ---**");
+    }
+}
