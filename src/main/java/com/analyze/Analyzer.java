9d16a9138f264c93ceabc2745372c0870862b194
Ken-Likit <88905005+Ken-Likit@users.noreply.github.com>
2023-11-18 19:51:11 -0600

    Comment and format changes

--- src/main/java/com/analyze/Analyzer.java
+++ src/main/java/com/analyze/Analyzer.java
@@ -1,21 +1,23 @@
 package com.analyze;
 
 import java.util.ArrayList;
+
 import com.input.LogData;
 
 public class Analyzer {
 
-    //logdata is logobject now
 
     private ArrayList<AnalysisTask> queue = new ArrayList<AnalysisTask>();
 
-    public LogData analyze(LogData analyzeLogObject) { //return output (new logData), analysistask already implemented check design dock for specifics
+    //Takes in LogData to execute a task. Returns LogData with results.
+    public LogData analyze(LogData analyzeLogObject) {
         for (AnalysisTask task : queue) {
             task.execute(analyzeLogObject);
         }
         return analyzeLogObject;
     }
 
+    //Adds a task to the queue. The task will be applied to a LogData object.
     public void addToQueue(AnalysisTask newTask) {
         queue.add(newTask);
     }
