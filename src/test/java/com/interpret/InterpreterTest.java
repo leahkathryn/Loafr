c0e476fb0e7f29e165c8c6a315d66b459bb86a74
leahkathryn <66423163+leahkathryn@users.noreply.github.com>
2023-11-27 12:11:54 -0600

    Numbering test cases and added a "testlogger" to build file so test pass/fail will appear when we run "gradle build"

--- src/test/java/com/interpret/InterpreterTest.java
+++ src/test/java/com/interpret/InterpreterTest.java
@@ -24,24 +24,24 @@ import java.util.Arrays;
 /**
  * This test class tests the public method getAnalysisTask of the Interpreter class.
  * Test cases:
- *      "search" keyword with valid instructions returns Search AnalysisTask
- *      "sort" keyword with valid instructions returns null
- *      "filter" keyword with valid instructions returns null
- *      undefined keyword with valid instructions returns null
+ *      Test 1: "search" keyword with valid instructions returns Search AnalysisTask
+ *      Test 2: "sort" keyword with valid instructions returns null
+ *      Test 3: "filter" keyword with valid instructions returns null
+ *      Test 4: undefined keyword with valid instructions returns null
  *
  *      All of the following test cases provide the "search" keyword:
- *      Valid regular expression as only element in instructions list returns Search AnalysisTask
- *      Valid attribute type (dataValue), DataID, and regular expression returns Search AnalysisTask
- *      Valid attribute type (timeStamp) and regular expression returns Search AnalysisTask
- *      Valid attribute type (event) and regular expression returns Search AnalysisTask
- *      Valid attribute type (dataType) and regular expression returns Search AnalysisTask
- *      Valid attribute type (dataID) and regular expression returns Search AnalysisTask
- *      Empty instructions list returns null
- *      Invalid attribute type returns null
- *      Invalid DataID when attribute type is "dataValue", returns null
- *      Invalid number of instructions in list when attribute type is "dataValue", returns null
- *      Invalid number of instructions in list when attribute type is not "dataValue", returns null
- *      The regular expression provided in the list is a blank String, returns null
+ *      Test 5: Valid regular expression as only element in instructions list returns Search AnalysisTask
+ *      Test 6: Valid attribute type (dataValue), DataID, and regular expression returns Search AnalysisTask
+ *      Test 7: Valid attribute type (timeStamp) and regular expression returns Search AnalysisTask
+ *      Test 8: Valid attribute type (event) and regular expression returns Search AnalysisTask
+ *      Test 9: Valid attribute type (dataID) and regular expression returns Search AnalysisTask
+ *      Test 10: Valid attribute type (dataType) and regular expression returns Search AnalysisTask
+ *      Test 11: Empty instructions list returns null
+ *      Test 12: Invalid attribute type returns null
+ *      Test 13: Invalid number of instructions in list when attribute type is "dataValue", returns null
+ *      Test 14: Invalid DataID when attribute type is "dataValue", returns null
+ *      Test 15: Invalid number of instructions in list when attribute type is not "dataValue", returns null
+ *      Test 16: The regular expression provided in the list is a blank String, returns null
  *
  * @author Leah Lehmeier
  */
@@ -64,7 +64,9 @@ public class InterpreterTest
         interpreter = new Interpreter(configuration);
     }
 
-    // test case naming convention: MethodName_StateUnderTest_ExpectedBehavior
+    /* * *
+     * Test 1
+     * * */
     @Test
     void GetAnalysisTask_SearchKeyword_ReturnSearchAnalysisTask()
     {
@@ -75,6 +77,9 @@ public class InterpreterTest
         assertInstanceOf(Search.class,task);
     }
 
+    /* * *
+     * Test 2
+     * * */
     @Test
     void GetAnalysisTask_SortKeyword_ReturnNull()
     {
@@ -84,6 +89,9 @@ public class InterpreterTest
         assertNull(task);
     }
 
+    /* * *
+     * Test 3
+     * * */
     @Test
     void GetAnalysisTask_FilterKeyword_ReturnNull()
     {
@@ -93,6 +101,9 @@ public class InterpreterTest
         assertNull(task);
     }
 
+    /* * *
+     * Test 4
+     * * */
     @Test
     void GetAnalysisTask_UndefinedKeyword_ReturnNull()
     {
@@ -102,6 +113,9 @@ public class InterpreterTest
         assertNull(task);
     }
 
+    /* * *
+     * Test 5
+     * * */
     @Test
     void GetAnalysisTask_ValidSearchEntireLogEntryInstruction_ReturnSearchAnalysisTask()
     {
@@ -143,6 +157,9 @@ public class InterpreterTest
         assertEquals("search_expr",task.getRegex());
     }
 
+    /* * *
+     * Test 6
+     * * */
     @Test
     void GetAnalysisTask_ValidSearchDataValueInstruction_ReturnSearchAnalysisTask()
     {
@@ -173,6 +190,9 @@ public class InterpreterTest
         assertEquals("search_expr",task.getRegex());
     }
 
+    /* * *
+     * Test 7
+     * * */
     @Test
     void GetAnalysisTask_ValidSearchTimeStampInstruction_ReturnSearchAnalysisTask()
     {
@@ -189,6 +209,9 @@ public class InterpreterTest
         assertEquals("search_expr",task.getRegex());
     }
 
+    /* * *
+     * Test 8
+     * * */
     @Test
     void GetAnalysisTask_ValidSearchEventTypeInstruction_ReturnSearchAnalysisTask()
     {
@@ -205,6 +228,9 @@ public class InterpreterTest
         assertEquals("search_expr",task.getRegex());
     }
 
+    /* * *
+     * Test 9
+     * * */
     @Test
     void GetAnalysisTask_ValidSearchDataIDInstruction_ReturnSearchAnalysisTask()
     {
@@ -221,6 +247,9 @@ public class InterpreterTest
         assertEquals("search_expr",task.getRegex());
     }
 
+    /* * *
+     * Test 10
+     * * */
     @Test
     void GetAnalysisTask_ValidSearchDataTypeInstruction_ReturnSearchAnalysisTask()
     {
@@ -237,6 +266,9 @@ public class InterpreterTest
         assertEquals("search_expr",task.getRegex());
     }
 
+    /* * *
+     * Test 11
+     * * */
     @Test
     void GetAnalysisTask_EmptyInstructions_ReturnNull()
     {
@@ -246,6 +278,9 @@ public class InterpreterTest
         assertNull(task);
     }
 
+    /* * *
+     * Test 12
+     * * */
     @Test
     void GetAnalysisTask_InvalidAttributeType_ReturnNull()
     {
@@ -255,6 +290,9 @@ public class InterpreterTest
         assertNull(task);
     }
 
+    /* * *
+     * Test 13
+     * * */
     @Test
     void GetAnalysisTask_DataValueAttributeTypeInvalidNumberOfInstructions_ReturnNull()
     {
@@ -264,6 +302,9 @@ public class InterpreterTest
         assertNull(task);
     }
 
+    /* * *
+     * Test 14
+     * * */
     @Test
     void GetAnalysisTask_DataValueAttributeTypeInvalidDataID_ReturnNull()
     {
@@ -273,6 +314,9 @@ public class InterpreterTest
         assertNull(task);
     }
 
+    /* * *
+     * Test 15
+     * * */
     @Test
     void GetAnalysisTask_OtherAttributeTypeInvalidNumberOfInstructions_ReturnNull()
     {
@@ -298,6 +342,9 @@ public class InterpreterTest
         assertNull(task);
     }
 
+    /* * *
+     * Test 16
+     * * */
     @Test
     void GetAnalysisTask_EmptyRegEx_ReturnNull()
     {
