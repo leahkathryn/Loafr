eedd16419beb07e4aaa67bff83485afa30e007bb
Ken-Likit <88905005+Ken-Likit@users.noreply.github.com>
2023-12-10 22:08:02 -0600

    Refactor execute function

--- src/main/java/com/analyze/Search.java
+++ src/main/java/com/analyze/Search.java
@@ -54,45 +54,95 @@ public class Search implements AnalysisTask {
         return regex;
     }
 
+    public <T> LogData noAttribute(Pattern pattern,  List<LogEvent> events) {
+        LogData output = new LogData();
+        HashMap<DataID, List<?>> data;
+        for (LogEvent event : events) {
+            if (event.getTimeStamp() != null) {
+                Matcher matcher = pattern.matcher(event.getTimeStamp().toString());
+                if (matcher.find()) {
+                    output.addLogEvent(event);
+                    continue;
+                }
+                Matcher matcher1 = pattern.matcher(event.getEventType());
+                if (matcher1.find()) {
+                    output.addLogEvent(event);
+                    continue;
+                }
+            }
+
+            data = event.getDataIDMap();
+            if(data != null) {
+                outerloop:
+                for (List<?> dataList : data.values()) { //parses through all DataiID keys
+                    for (Object dataValue : dataList) { //parses list for each dataId
+                        Matcher matcher2 = pattern.matcher(dataValue.toString());
+                        if (matcher2.find()) {
+                            output.addLogEvent(event);
+                            break outerloop; //breaks loop since event has been added
+                        }
+                    }
+                }
+            }
+        }
+        return output;
+    }
+
+    public <T> LogData hasAttribute(Pattern pattern, List<LogEvent> events) {
+        LogData output = new LogData();
+        HashMap<DataID, List<?>> data;
+
+        for (LogEvent event : events) {
+            //Pattern pattern = Pattern.compile(regex);
+            switch (attributeType) {
+                case TIMESTAMP -> { //Checks if timestamp matches regex
+                    Matcher matcher = pattern.matcher(event.getTimeStamp().toString());
+                    if (matcher.find()) {
+                        output.addLogEvent(event);
+                    }
+                }
+                case EVENT -> { //Checks if event matches regex
+                    Matcher matcher1 = pattern.matcher(event.getEventType());
+                    if (matcher1.find()) {
+                        output.addLogEvent(event);
+                    }
+                }
+                case DATAID -> { //Checks if DataID keys match regex
+                    data = event.getDataIDMap();
+                    for (DataID dataKey : data.keySet()) {
+                        Matcher matcher2 = pattern.matcher(dataKey.toString());
+                        if (matcher2.find()) {
+                            output.addLogEvent(event);
+                            break;
+                        }
+                    }
+                }
+                case DATATYPE -> { //Checks if Data type matches regex
+                    data = event.getDataIDMap();
+                    for (DataID dataKey : data.keySet()) {
+                        Matcher matcher3 = pattern.matcher(dataKey.getType().toString());
+                        if (matcher3.find()) {
+                            output.addLogEvent(event);
+                            break;
+                        }
+                    }
+                }
+                default -> ErrorHandler.logError("The requested attribute type is not implemented by Loafr.");
+            }
+        }
+        return output;
+    }
+
     //Executes a search function on the inputted LogData.
     //Takes in a LogData object and returns a LogData object.
     @Override
     public <T> LogData execute(LogData logObject) {
         LogData output = new LogData();
-        HashMap<DataID, List<?>> data;
         events = logObject.getEventList();
-
         //Searches all fields in the LogEvent.
         Pattern pattern = Pattern.compile(regex);
         if (null == attributeType) {
-            for (LogEvent event : events) {
-                if (event.getTimeStamp() != null) {
-                    Matcher matcher = pattern.matcher(event.getTimeStamp().toString());
-                    if (matcher.find()) {
-                        output.addLogEvent(event);
-                        continue;
-                    }
-                    Matcher matcher1 = pattern.matcher(event.getEventType());
-                    if (matcher1.find()) {
-                        output.addLogEvent(event);
-                        continue;
-                    }
-                }
-
-                data = event.getDataIDMap();
-                if(data != null) {
-                    outerloop:
-                    for (List<?> dataList : data.values()) { //parses through all DataiID keys
-                        for (Object dataValue : dataList) { //parses list for each dataId
-                            Matcher matcher2 = pattern.matcher(dataValue.toString());
-                            if (matcher2.find()) {
-                                output.addLogEvent(event);
-                                break outerloop; //breaks loop since event has been added
-                            }
-                        }
-                    }
-                }
-            }
+            output = noAttribute(pattern, events);
         } else if (LogEvent.AttributeType.DATAVALUE == attributeType) { //Searches all DataID fields.
             for (LogEvent event : events) {
                 HashMap<DataID, List<?>> dataMap = event.getDataIDMap();
@@ -107,45 +157,8 @@ public class Search implements AnalysisTask {
             }
         } else {
             // Searches the attributeType field specified in the script
-            for (LogEvent event : events) {
-                //Pattern pattern = Pattern.compile(regex);
-                switch (attributeType) {
-                    case TIMESTAMP -> { //Checks if timestamp matches regex
-                        Matcher matcher = pattern.matcher(event.getTimeStamp().toString());
-                        if (matcher.find()) {
-                            output.addLogEvent(event);
-                        }
-                    }
-                    case EVENT -> { //Checks if event matches regex
-                        Matcher matcher1 = pattern.matcher(event.getEventType());
-                        if (matcher1.find()) {
-                            output.addLogEvent(event);
-                        }
-                    }
-                    case DATAID -> { //Checks if DataID keys match regex
-                        data = event.getDataIDMap();
-                        for (DataID dataKey : data.keySet()) {
-                            Matcher matcher2 = pattern.matcher(dataKey.toString());
-                            if (matcher2.find()) {
-                                output.addLogEvent(event);
-                                break;
-                            }
-                        }
-                    }
-                    case DATATYPE -> { //Checks if Data type matches regex
-                        data = event.getDataIDMap();
-                        for (DataID dataKey : data.keySet()) {
-                            Matcher matcher3 = pattern.matcher(dataKey.getType().toString());
-                            if (matcher3.find()) {
-                                output.addLogEvent(event);
-                                break;
-                            }
-                        }
-                    }
-                    default -> ErrorHandler.logError("The requested attribute type is not implemented by Loafr.");
-                }
-            }
-        }
+            output = hasAttribute(pattern, events);
+       }
         return output;
     }
 }
